d3ad205f598d870536844c8fbe61a4cae9fa4962
Axely7 <axely7@hotmail.com>
2022-11-15 10:41:27 -0600

    receving body from request and query params in routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,32 @@
-const { response } = require("express");
+const { response, request } = require("express");
 
-const userGet = (req, res = response) => {
+const userGet = (req = request, res = response) => {
+  const { q, nombre, page = 1 } = req.query;
+
   res.json({
     msg: "get API - controlador",
+    q,
+    nombre,
+    page,
   });
 };
 
 const userPut = (req, res = response) => {
+  const id = req.params.id;
+
   res.json({
     msg: "put API - controlador",
+    id,
   });
 };
 
 const userPost = (req, res = response) => {
+  const { nombre, edad } = req.body;
+
   res.json({
     msg: "post API - controlador",
+    nombre,
+    edad,
   });
 };
 
