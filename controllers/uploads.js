52a209d688cac682957e1f7e62ad925574f2e0c8
Axel Jimenez <axely7@hotmail.com>
2023-01-11 10:29:39 -0600

    fileupload

---
+++ controllers/uploads.js
@@ -0,0 +1,44 @@
+const { response } = require("express");;
+const path = require('path')
+
+
+const cargarArchivo = (req, res = response) => {
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+        res.status(400).json('No hay archivos que subir.');
+        return;
+    }
+
+    const { archivo } = req.files;
+
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length - 1]
+
+    // Validar la extensión
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+
+    if(!extensionesValidas.includes(extension)){
+        return res.status(400).json({
+            msg: `La extensión ${extension} no es permitida, ${extensionesValidas}`
+        })
+    }
+
+    console.log({extension})
+
+
+
+    // const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+
+    // archivo.mv(uploadPath, (err) => {
+    //     if (err) {
+    //     return res.status(500).json({err});
+    //     }
+
+    //     res.json({msg: 'File uploaded to ' + uploadPath});
+    // });
+}
+
+
+module.exports = {
+    cargarArchivo
+}
