a755094512d2d31d2f6d689dc6029e2e86c97615
Axel Jimenez <axely7@hotmail.com>
2023-01-17 11:29:39 -0600

    updating images, getting images and showing default image

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,11 +1,11 @@
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
 const { Usuario, Producto } = require('../models');
-const usuario = require("../models/usuario");
+const path = require('path');
+const fs = require('fs')
 
 const cargarArchivo = async(req, res = response) => {
 
-   
     try {
 
         // Imagenes
@@ -51,6 +51,17 @@ const actualizarImagen = async(req, res = response) => {
             res.status(500).json({msg: 'No existe colección disponible'})
     }
 
+    // limpiar imágenes previas
+    if(modelo.img){
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img)
+        if(fs.existsSync(pathImagen)){
+            fs.unlinkSync(pathImagen)
+        }
+    }
+
+
+
     const nombre = await subirArchivo(req.files, undefined, coleccion);
     modelo.img = nombre;
 
@@ -61,7 +72,55 @@ const actualizarImagen = async(req, res = response) => {
 }
 
 
+const mostrarImagen = async(req, res = response) => {
+
+    const { id, coleccion } = req.params;
+  
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if(!modelo){
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+        break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if(!modelo){
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+        break;
+            
+    
+        default:
+            res.status(500).json({msg: 'No existe colección disponible'})
+    }
+
+    // limpiar imágenes previas
+    if(modelo.img){
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img)
+        if(fs.existsSync(pathImagen)){
+            return res.sendFile(pathImagen)
+        }
+    }
+
+    const pathDefaultImage = path.join(__dirname, '../assets', 'no-image.jpg')
+    res.sendFile(pathDefaultImage);
+
+}
+
+
+
+
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagen,
+    mostrarImagen
 }
