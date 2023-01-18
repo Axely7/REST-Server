ab65a538662147b37db38a20ef26ce0b0bd51de1
Axel Jimenez <axely7@hotmail.com>
2023-01-18 10:48:40 -0600

    files uploaded to cloudinary

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require("../controllers/uploads");
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require("../controllers/uploads");
 const { coleccionesPermitidas } = require("../helpers");
 const { validarCampos, validarArchivoSubir } = require("../middlewares");
 
@@ -15,7 +15,10 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe ser de mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarImagen)
+], 
+// actualizarImagen,
+actualizarImagenCloudinary
+)
 
 router.get('/:coleccion/:id', [
     check('id', 'El id debe ser de mongo').isMongoId(),
